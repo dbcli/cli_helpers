1e6fcb134f55cb70ddd394a051a86c45aa50c944
Thomas Roten <thomas@roten.us>
2017-04-18 10:10:06 -0500

    Make whitespace and numparse configurable.

--- cli_helpers/tabular_output/tabulate_adapter.py
+++ cli_helpers/tabular_output/tabulate_adapter.py
@@ -1,8 +1,6 @@
 from cli_helpers.packages import tabulate
 from .preprocessors import bytes_to_string, align_decimals
 
-tabulate.PRESERVE_WHITESPACE = True
-
 supported_markup_formats = ('mediawiki', 'html', 'latex', 'latex_booktabs',
                             'textile', 'moinmoin', 'jira')
 supported_table_formats = ('plain', 'simple', 'grid', 'fancy_grid', 'pipe',
@@ -12,11 +10,14 @@ supported_formats = supported_markup_formats + supported_table_formats
 preprocessors = (bytes_to_string, align_decimals)
 
 
-def adapter(data, headers, table_format=None, missing_value='', **_):
+def adapter(data, headers, table_format=None, missing_value='',
+            disable_numparse=True, preserve_whitespace=True, **_):
     """Wrap tabulate inside a function for TabularOutputFormatter."""
     kwargs = {'tablefmt': table_format, 'missingval': missing_value,
-              'disable_numparse': True}
+              'disable_numparse': disable_numparse}
     if table_format in supported_markup_formats:
         kwargs.update(numalign=None, stralign=None)
+
+    tabulate.PRESERVE_WHITESPACE = preserve_whitespace
 
     return tabulate.tabulate(data, headers, **kwargs)
