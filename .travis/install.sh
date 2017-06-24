90c3e93be37772d2e223bfd9f52679a3406e0948
Thomas Roten <tsr@lavabit.com>
2017-06-24 08:14:46 -0500

    Add install script for travis to use.

---
+++ .travis/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    sw_vers
+
+    git clone --depth 1 https://github.com/pyenv/pyenv ~/.pyenv
+    PYENV_ROOT="$HOME/.pyenv"
+    PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+
+    case "${TOXENV}" in
+        py27)
+            curl -O https://bootstrap.pypa.io/get-pip.py
+            python get-pip.py --user
+            ;;
+        py36)
+            pyenv install 3.6.1
+            pyenv global 3.6.1
+            ;;
+    esac
+    pyenv rehash
+    python -m pip install --user virtualenv
+fi
+
+python -m virtualenv ~/.venv
+source ~/.venv/bin/activate
+pip install tox
