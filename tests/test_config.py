5cb7af6836d680afbb48ab256023f53e86b03e00
Thomas Roten <tsr@tundrius.net>
2018-10-26 02:45:03 +0000

    Remove debugging line of code.

--- tests/test_config.py
+++ tests/test_config.py
@@ -198,7 +198,6 @@ def test_write_and_read_default_config_from_configspec(temp_dir=None):
         temp_dir, APP_NAME, APP_AUTHOR, config_file, default=default_file, validate=True
     )
     config.read_default_config()
-    assert config.default_config.encoding == 'utf8'
     config.write_default_config()
 
     user_config = _mocked_user_config(
